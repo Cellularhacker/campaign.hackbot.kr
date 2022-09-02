3741b476e0683cd93ca230417d3dfb30ab9b3dc5
Cellularhacker <wva3cdae@gmail.com>
2022-09-02 12:11:48 +0900

    Init page

---
+++ src/routes/Prevention.svelte
@@ -0,0 +1,26 @@
+<div id="prevention">
+    <div class="api-title-area">
+        <h2>생명사랑 캠페인</h2>
+    </div>
+    <div class="api-cs-wrap">
+        <div class="advice">
+            <div class="tit">
+                <h4>"당신은 "</h4>
+                <strong>소중한 사람입니다.</strong>
+            </div>
+        </div>
+        <div class="cont-lk">
+
+        </div>
+        <div class="help-lk">
+
+        </div>
+    </div>
+</div>
+
+<style>
+    #prevention{
+        width: 1024px;
+        height: 600px;
+    }
+</style>
